0d8b3c5d1a8c5aeac38117fe8b727a8e28ce093f
masayuki mizokami <kasuteraman0411@gmail.com>
2021-10-13 04:35:23 +0000

    演算子完成

--- den.maiin2.js
+++ den.maiin2.js
@@ -1,3 +1,4 @@
+
 let num = "";
 let key = "";
 let sliceEnd = ""
@@ -6,17 +7,8 @@ let keyP = ""
 let keyPlus =""
 
 function append( _d )  {
-  if(sliceEnd == "+"||sliceEnd == "-"||sliceEnd == "*"||sliceEnd == "/"||sliceEnd == "."){
-      if(keyPlus == "+"){//(key == "+"||key == "-"||key == "*"||key == "/"||key == "."){
-        num = sliceFirst+_d
-      }else{
-         num += ""+_d
-    }
- }else if(sliceEnd == "0"||sliceEnd == "00"){
-          num += ""+_d
-        
-  }else if(sliceEnd == ""){
-       num += ""+_d
+  if(sliceEnd == "0"||sliceEnd == "00"){
+          num = ""+_d
   }else{
     num += ""+_d
   }     
@@ -33,22 +25,29 @@ function append( _d )  {
     
     
     
-   // if(key =="+"||key<0){
-       // if(sliceEnd == "+"||sliceEnd == "-"||sliceEnd == "*"||sliceEnd == "/"||sliceEnd == "."){
-       //    num += ""+_d;
-     //    }else{
-       //     num = sliceFirst + _d; 
-      //   }
-//    }else if(key  == "0" || key == "00"){
-  //        if(num == "0"|| num == "00"){
-    //        num = ""+_d;
-      //    }else{
-        //    num += ""+_d;
-        //  }  
-  //  }else if(key <10 && key>=0){      
-    //      num += ""+_d;
-//    }else{
-   //    num = sliceFirst + _d; 
+    function symbol( _d )  {
+  if(sliceEnd == "+"||sliceEnd == "-"||sliceEnd == "*"||sliceEnd == "/"||sliceEnd == "."){
+        num = sliceFirst+_d
+  }else if(sliceEnd == ""){
+       num = ""+_d
+  }else{
+    num += ""+_d
+  }     
+    document.querySelector( 'input' ).value = num
+    key = _d
+    keyP = num + _d+_d
+    keyPlus = keyP.substr( -1,1)
+     sliceEnd = num.substr( -1,1)
+     sliceFirst = num.substr(0,num.length -1)
+     sliceEnd2 = sliceFirst.substr( -1,1)
+     sliceFirst2 = num.substr(0,num.length -2)
+    }
+    
+    
+    
+    
+    
+   
     
 
     function calc() {
