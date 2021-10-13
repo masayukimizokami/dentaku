724f5da935a25cc41588fdb601aa14eecf988d33
masayuki mizokami <kasuteraman0411@gmail.com>
2021-10-13 04:40:24 +0000

    js整列

--- den.maiin2.js
+++ den.maiin2.js
@@ -8,55 +8,45 @@ let keyPlus =""
 
 function append( _d )  {
   if(sliceEnd == "0"||sliceEnd == "00"){
-          num = ""+_d
+     num = ""+_d
   }else{
-    num += ""+_d
+     num += ""+_d
   }     
     document.querySelector( 'input' ).value = num
-    key = _d
-    keyP = num + _d+_d
-    keyPlus = keyP.substr( -1,1)
+     key = _d
+     keyP = num + _d+_d
+     keyPlus = keyP.substr( -1,1)
      sliceEnd = num.substr( -1,1)
      sliceFirst = num.substr(0,num.length -1)
      sliceEnd2 = sliceFirst.substr( -1,1)
      sliceFirst2 = num.substr(0,num.length -2)
     }
     
-    
-    
-    
-    function symbol( _d )  {
+function symbol( _d )  {
   if(sliceEnd == "+"||sliceEnd == "-"||sliceEnd == "*"||sliceEnd == "/"||sliceEnd == "."){
-        num = sliceFirst+_d
+     num = sliceFirst+_d
   }else if(sliceEnd == ""){
-       num = ""+_d
+     num = ""+_d
   }else{
-    num += ""+_d
+     num += ""+_d
   }     
     document.querySelector( 'input' ).value = num
-    key = _d
-    keyP = num + _d+_d
-    keyPlus = keyP.substr( -1,1)
+     key = _d
+     keyP = num + _d+_d
+     keyPlus = keyP.substr( -1,1)
      sliceEnd = num.substr( -1,1)
      sliceFirst = num.substr(0,num.length -1)
      sliceEnd2 = sliceFirst.substr( -1,1)
      sliceFirst2 = num.substr(0,num.length -2)
     }
-    
-    
-    
-    
-    
-   
-    
 
-    function calc() {
+  function calc() {
         num=eval(num)
         num=String(num)
         document.querySelector( 'input' ).value = num
     }
     
-   function update()  {
+  function update()  {
            document.rstBtn.reset() ;
     }
     
